8f24d046119fbf96554e104bc992692cb5142134
jnishime <r.bucknumber8@gmail.com>
2020-05-31 10:36:58 +0900

    Modified profile create / Add profile table

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -26,19 +26,20 @@
                     <div class="form-group row">
                         <label class="col-md-2">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('gender') }}">
+                            <input name="gender" type="radio" value="男性" {{ old('gender') == '男性' ? 'checked' : '' }}>男性<br />
+                            <input name="gender" type="radio" value="女性" {{ old('gender') == '女性' ? 'checked' : '' }}>女性
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('hobby') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     {{ csrf_field() }}
