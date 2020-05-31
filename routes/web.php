47ca4f7766dbc19d5f93a9ece00b40a8d3b501c6
jnishime <r.bucknumber8@gmail.com>
2020-05-31 09:35:59 +0900

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,13 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
   // NewsController
-  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 
   // ProfileController
   Route::get('profile/create', 'Admin\ProfileController@add');
   Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
